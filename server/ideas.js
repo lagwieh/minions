98346c806456a500188341024a229bf641c0c6ed
lagwieh <lagwieh@hotmail.com>
2024-01-15 02:20:19 -0300

    basic idea and meeting methods

--- server/ideas.js
+++ server/ideas.js
@@ -3,11 +3,70 @@ const db = require('./db');
 const ideaRouter = express.Router();
 const ideas = db.getAllFromDatabase('ideas')
 
+ideaRouter.param('ideaId', (req, res, next, id) => {
+  let ideaId = id
+  try {
+    const ideaIdExists = ideas.find(idea => {
+      return idea.id == ideaId;
+    });
+    if (ideaIdExists) {
+      req.ideaId = ideaId;
+      next();
+    } else {
+      next(new Error('IDEA NOT FOUND'));
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 // GET METHOD
-  // GET ALL IDEAS
-ideaRouter.get('/', (req, res, next)=>{
+// GET ALL IDEAS
+ideaRouter.get('/', (req, res, next) => {
   // console.log('ideas request arrived');
   res.send(ideas)
 });
+//GET IDEA BY ID
+ideaRouter.get('/:ideaId', (req, res, next) => {
+  const idea = db.getFromDatabaseById('ideas', req.ideaId)
+  res.send(idea)
+});
+// POST METHOD
+ideaRouter.post('/', (req, res, next) => {
 
+
+  try {
+    const idea = req.body;
+    // console.log(Object.keys(meeting))
+    if (Object.keys(idea) != 0) {
+      idea.name = idea.name || '';
+      idea.description = idea.description || '';
+      idea.numWeeks = idea.numWeeks || 0;
+      idea.weeklyRevenue = idea.weeklyRevenue || 0;
+      db.addToDatabase('ideas', idea)
+      res.status(201).send(idea)
+    }
+  } catch (err) {
+    next(err);
+  }
+
+});
+// PUT METHOD
+// MODIFY IDEA
+ideaRouter.put('/:minionId', (req, res, next) => {
+
+  ideaUpdated = db.updateInstanceInDatabase('ideas', req.body);
+  res.send(ideaUpdated);
+
+});
+// DELETE METHOD
+ideaRouter.delete('/:ideaId',(req, res, next)=>{
+  const isDeleted = db.deleteFromDatabasebyId('ideas',req.ideaId)
+  res.status(204).send()
+} );
+
+// ERROR MIDDLEWARE
+ideaRouter.use((err, req, res, next) => {
+  let status = err.status || 500;
+  res.status(status).send(err.message)
+});
 module.exports.ideaRouter = ideaRouter;
--- server/meetings.js
+++ server/meetings.js
@@ -1,12 +1,72 @@
 const express = require('express');
 const db = require('./db');
 const meetingRouter = express.Router();
-const meetings = db.getAllFromDatabase('meetings')
+let meetings = db.getAllFromDatabase('meetings')
+// console.log(meetings)
+// PARAM ROUTER
+meetingRouter.param('meetingId', (req, res, next, id)=>{
+  console.log(id)
+  let meetingId = id
+  try {
+    const meetingIdExists = meetings.find(meeting=>{
+      return meeting.id == meetingId;
+    });
+    if(meetingIdExists){
+      req.meetingId = meetingId;
+      next();
+    }else{
+      next(new Error('MEETING NOT FOUND'));
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 // GET METHOD
   // GET ALL MEETINGS
 meetingRouter.get('/', (req, res, next)=>{
   // console.log('meetings request arrived');
-  res.send(meetings)
+  res.send(meetings);
+});
+  // GET MEETING BY ID
+meetingRouter.get('/:meetingId', (req, res, next)=>{
+  const meeting = db.getFromDatabaseById('minions', req.meetingId);
+  res.send(meeting);
+});
+// POST METHOD
+meetingRouter.post('/', (req, res, next)=>{
+  
+ 
+  try {
+    const meeting = req.body;
+    // console.log(Object.keys(meeting))
+    if(Object.keys(meeting)!=0){
+      meeting.time = meeting.time || '';
+      meeting.date = meeting.date || new Date();
+      meeting.day = meeting.day || '';
+      meeting.note = meeting.note || '';
+      db.addToDatabase('meetings',meeting)
+      res.status(201).send(meeting)
+    }else{
+      const newRandomMeeting = db.createMeeting()
+      // console.log(meetings)
+      // res.send(newRandomMeeting)
+    }
+  } catch (error) {
+    next(err);
+  }
+
+});
+// DELETE METHOD
+  // DELETE ALL MEETINGS
+meetingRouter.delete('/', (req, res, next)=>{
+  const deletedDb = db.deleteAllFromDatabase('meetings');
+  meetings = deletedDb;
+  res.status(204).send();
+});
+// ERROR MIDDLEWARE
+meetingRouter.use((err, req, res, next)=>{
+  let status = err.status || 500;
+  res.status(status).send(err.message)
 });
 
 module.exports.meetingRouter = meetingRouter;
